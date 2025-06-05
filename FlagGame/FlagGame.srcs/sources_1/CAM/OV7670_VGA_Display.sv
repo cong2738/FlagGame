9c0a8008fc8ca6830d0aee31b2fc83a146b8368b
pumpk <cong2738@gmail.com>
2025-06-05 15:07:39 +0900

    청기백기 게임 프로젝트 MERGE:  - 유저 깃발 판단 모듈  - 랜덤 명령어 발생 모듈  - OV7670캠 입력 VGA 출력 모듈  - OV7670캠 설정 WRITE SCCB 모듈

---
+++ FlagGame/FlagGame.srcs/sources_1/CAM/OV7670_VGA_Display.sv
@@ -0,0 +1,87 @@
+`timescale 1ns / 1ps
+
+module OV7670_VGA_Display (
+    // global signals
+    input  logic       clk,
+    input  logic       reset,
+    // ov7670 signals
+    output logic       ov7670_xclk,    // == mclk
+    input  logic       ov7670_pclk,
+    input  logic       ov7670_href,
+    input  logic       ov7670_v_sync,
+    input  logic [7:0] ov7670_data,
+    // export signals
+    output logic       Hsync,
+    output logic       Vsync,
+    output logic [3:0] vgaRed,
+    output logic [3:0] vgaGreen,
+    output logic [3:0] vgaBlue,
+    output logic [9:0] x_pixel,
+    output logic [9:0] y_pixel
+);
+
+    logic        we;
+    logic [16:0] wAddr;
+    logic [15:0] wData;
+    logic [16:0] rAddr;
+    logic [15:0] rData;
+
+    logic        DE;
+    logic        w_rclk;
+    logic        rclk;
+    logic        oe;
+
+
+    VGA_Controller U_VGAController (
+        .clk    (clk),
+        .reset  (reset),
+        .rclk   (w_rclk),
+        .h_sync (Hsync),
+        .v_sync (Vsync),
+        .DE     (DE),
+        .x_pixel(x_pixel),
+        .y_pixel(y_pixel)
+    );
+
+    pixel_clk_gen U_OV7670_Clk_Gen (
+        .clk  (clk),
+        .reset(reset),
+        .pclk (ov7670_xclk)
+    );
+
+    OV7670_MemController U_OV7670_MemController (
+        .pclk       (ov7670_pclk),
+        .reset      (reset),
+        .href       (ov7670_href),
+        .v_sync     (ov7670_v_sync),
+        .ov7670_data(ov7670_data),
+        .we         (we),
+        .wAddr      (wAddr),
+        .wData      (wData)
+    );
+
+    frame_buffer U_Frame_Buffer (
+        .wclk (ov7670_pclk),
+        .we   (we),
+        .wAddr(wAddr),
+        .wData(wData),
+        .rclk (rclk),
+        .oe   (oe),
+        .rAddr(rAddr),
+        .rData(rData)
+    );
+
+    QVGA_MemController U_QVGA_MemController (
+        .clk       (w_rclk),
+        .x_pixel   (x_pixel),
+        .y_pixel   (y_pixel),
+        .DE        (DE),
+        .rclk      (rclk),
+        .d_en      (oe),
+        .rAddr     (rAddr),
+        .rData     (rData),
+        .red_port  (vgaRed),
+        .green_port(vgaGreen),
+        .blue_port (vgaBlue)
+    );
+endmodule
