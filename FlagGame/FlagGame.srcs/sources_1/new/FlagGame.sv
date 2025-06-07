0f1d07b6427bf97c95e063b1cd2b5ee9c9833714
pumpk <cong2738@gmail.com>
2025-06-07 17:54:52 +0900

    USER 신호 처리 로직 수정: 비트 연산에서 비교 연산으로 변경

--- FlagGame/FlagGame.srcs/sources_1/new/FlagGame.sv
+++ FlagGame/FlagGame.srcs/sources_1/new/FlagGame.sv
@@ -73,9 +73,9 @@ module FlagGame (
         end
     end
 
-    logic user_blue_up = USER & 2'b01;
-    logic user_white_up = USER & 2'b01;
-    logic user_both_up = user_blue_up & user_white_up;
+    logic user_blue_up = USER[0] == 1;
+    logic user_white_up = USER[1] == 1;
+    logic user_both_up = user_blue_up && user_white_up;
     always_comb begin : GAME_NEXT_LOGIC
         game_next     = game_state;
         temp_CMD_next = temp_CMD;
