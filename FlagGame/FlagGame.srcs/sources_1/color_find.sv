b94fee97a1cabdb43ee2a9962d81b9cae6162aff
pumpk <cong2738@gmail.com>
2025-06-08 18:06:40 +0900

    신호 처리 로직 수정: 사용자 핸들 출력을 USER로 변경하고 카운터 변수 정리

--- FlagGame/FlagGame.srcs/sources_1/color_find.sv
+++ FlagGame/FlagGame.srcs/sources_1/color_find.sv
@@ -8,39 +8,38 @@ module color_find (
     input [3:0] R,
     input [3:0] G,
     input [3:0] B,
-    output logic user_hand0,
-    output logic user_hand1
+    output logic[1:0] USER
 );
-    reg [31:0] target0_U_count;
-    reg [31:0] target0_D_count;
-    reg [31:0] target1_U_count;
-    reg [31:0] target1_D_count;
-    reg        blue_flag = (R < 5) && (G < 5) && (B > 10);
+    reg [31:0] blue_flag_U_count;
+    reg [31:0] blue_flag_D_count;
+    reg [31:0] red_flag_U_count;
+    reg [31:0] red_flag_D_count;
     reg        red_flag = (R > 10) && (G < 5) && (B < 5);
+    reg        blue_flag = (R < 5) && (G < 5) && (B > 10);
     always_ff @(posedge clk, posedge reset) begin : COLOR_FIND
         if (reset) begin
-            target0_U_count <= 0;
-            target0_D_count <= 0;
-            target1_U_count <= 0;
-            target1_D_count <= 0;
+            blue_flag_U_count <= 0;
+            blue_flag_D_count <= 0;
+            red_flag_U_count <= 0;
+            red_flag_D_count <= 0;
         end else begin
             if (x_pixel == 320 && y_pixel == 240) begin
-                user_hand1 <= (target1_D_count < target1_U_count);
-                user_hand0 <= (target0_D_count < target0_U_count);
+                USER[1] <= (red_flag_D_count < red_flag_U_count);
+                USER[0] <= (blue_flag_D_count < blue_flag_U_count);
             end else if (x_pixel == 0 && y_pixel == 0) begin
-                target0_U_count <= 0;
-                target0_D_count <= 0;
-                target1_U_count <= 0;
-                target1_D_count <= 0;
-            end else begin  //오른손
+                blue_flag_U_count <= 0;
+                blue_flag_D_count <= 0;
+                red_flag_U_count <= 0;
+                red_flag_D_count <= 0;
+            end else begin  
                 if (blue_flag) begin  //파랑
                     if (y_pixel < 120)  //반절 위면
-                        target0_U_count <= target0_U_count + 1;
-                    else target0_D_count <= target0_D_count + 1;
+                        blue_flag_U_count <= blue_flag_U_count + 1;
+                    else blue_flag_D_count <= blue_flag_D_count + 1;
                 end else if (red_flag) begin  //빨강
                     if (y_pixel < 120)  //반절 위면
-                        target1_U_count <= target1_U_count + 1;
-                    else target1_D_count <= target1_D_count + 1;
+                        red_flag_U_count <= red_flag_U_count + 1;
+                    else red_flag_D_count <= red_flag_D_count + 1;
                 end
             end
         end
