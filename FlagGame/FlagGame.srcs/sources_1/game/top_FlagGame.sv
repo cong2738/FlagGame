5cc80fa1dc206bdd9bc1da7afa394f17cc7cf895
pumpk <cong2738@gmail.com>
2025-06-09 19:52:02 +0900

    feat: 게임 모듈에 스피커 출력 추가 및 코드 정리

--- FlagGame/FlagGame.srcs/sources_1/game/top_FlagGame.sv
+++ FlagGame/FlagGame.srcs/sources_1/game/top_FlagGame.sv
@@ -8,7 +8,7 @@ module top_FlagGame (
     input  logic       ov7670_start,
     output logic       ov7670_scl,
     output logic       ov7670_sda,
-    output logic       ov7670_xclk,    // == mclk
+    output logic       ov7670_xclk,
     input  logic       ov7670_pclk,
     input  logic       ov7670_href,
     input  logic       ov7670_v_sync,
@@ -20,13 +20,19 @@ module top_FlagGame (
     output logic [3:0] vgaGreen,
     output logic [3:0] vgaBlue,
     output logic [3:0] fndCom,
-    output logic [7:0] fndFont
+    output logic [7:0] fndFont,
+    output logic       speaker
 );
     logic [3:0] Red, Green, Blue;
     logic [9:0] x_pixel, y_pixel;
     logic [3:0] ov7670_Red, ov7670_Green, ov7670_Blue;
+    logic [3:0] TEXT_Red, TEXT_Green, TEXT_Blue;
     logic [3:0] GAME;
+    logic [1:0] USER;
     logic [31:0] game_score, game_count;
+    logic [31:0] fndData = (USER[1] ? 10 : 0) + USER[0];
+
+    assign {vgaRed, vgaGreen, vgaBlue} = {Red, Green, Blue};
 
     SCCB_core u_OV7670_SCCB_core (
         .clk          (clk),
@@ -47,8 +53,8 @@ module top_FlagGame (
         .ov7670_Red   (ov7670_Red),
         .ov7670_Green (ov7670_Green),
         .ov7670_Blue  (ov7670_Blue),
-        .ov7670_en    (d_en),
-        .vga_en       (),
+        .ov7670_en    (),
+        .vga_en       (d_en),
         .Hsync        (Hsync),
         .Vsync        (Vsync),
         .x_pixel      (x_pixel),
@@ -67,12 +73,21 @@ module top_FlagGame (
         .i_red       (ov7670_Red),
         .i_green     (ov7670_Green),
         .i_blue      (ov7670_Blue),
-        .o_red       (Red),
-        .o_green     (Green),
-        .o_blue      (Blue)
+        .o_red       (TEXT_Red),
+        .o_green     (TEXT_Green),
+        .o_blue      (TEXT_Blue)
     );
 
-    logic [1:0] USER;
+    Gameover_effect u_Gameover_effect (
+        .gameover    (GAME),
+        .red_port_i  (TEXT_Red),
+        .green_port_i(TEXT_Green),
+        .blue_port_i (TEXT_Blue),
+        .red_port_o  (Red),
+        .green_port_o(Green),
+        .blue_port_o (Blue)
+    );
+
     game u_game (
         .clk       (clk),
         .reset     (reset),
@@ -83,10 +98,10 @@ module top_FlagGame (
         .GAME      (GAME),
         .USER      (USER),
         .game_count(game_count),
-        .game_score(game_score)
+        .game_score(game_score),
+        .speaker   (speaker)
     );
 
-    logic [31:0] fndData = (USER[1] ? 10 : 0) + USER[0];
     fndController u_fndController (
         .clk    (clk),
         .reset  (reset),
@@ -96,33 +111,32 @@ module top_FlagGame (
         .fndFont(fndFont)
     );
 
-    assign {vgaRed, vgaGreen, vgaBlue} = {Red, Green, Blue};
 endmodule
 
 module game (
-    input clk,
-    input reset,
-    input start,
-    input logic [9:0] x_pixel,
-    input logic [9:0] y_pixel,
-    input logic [11:0] color,
-    output logic [3:0] GAME,
-    output logic [1:0] USER,
+    input               clk,
+    input               reset,
+    input               start,
+    input  logic [ 9:0] x_pixel,
+    input  logic [ 9:0] y_pixel,
+    input  logic [11:0] color,
+    output logic [ 3:0] GAME,
+    output logic [ 1:0] USER,
     output logic [31:0] game_count,
-    output logic [31:0] game_score
+    output logic [31:0] game_score,
+    output logic        speaker
 );
     logic         seed_en;
     logic [127:0] seed;
     logic [ 31:0] rnd;
     logic [  3:0] flag_cmd;
 
-    xorshift128 u_xorshift128 (
-        .clk    (clk),
-        .rst    (reset),
-        .ce     (get),
-        .seed_en(seed_en),
-        .seed   (seed),
-        .rnd    (rnd)
+    PRNG u_PRNG (
+        .clk     (clk),
+        .rst     (reset),
+        .ce      (get),
+        .seed_btn(start),
+        .rnd     (rnd)
     );
 
     Flag_cmd u_Flag_cmd (
@@ -133,14 +147,14 @@ module game (
     );
 
     color_find u_color_find (
-        .clk       (clk),
-        .reset     (reset),
-        .x_pixel   (x_pixel),
-        .y_pixel   (y_pixel),
-        .R         (color[11:8]),
-        .G         (color[7:4]),
-        .B         (color[3:0]),
-        .USER(USER)
+        .clk    (clk),
+        .reset  (reset),
+        .x_pixel(x_pixel),
+        .y_pixel(y_pixel),
+        .R      (color[11:8]),
+        .G      (color[7:4]),
+        .B      (color[3:0]),
+        .USER   (USER)
     );
 
     FlagGame u_FlagGame (
@@ -155,4 +169,31 @@ module game (
         .game_score(game_score)
     );
 
+    edgeDT u_GAMEOVER_edge (
+        .clk  (clk),
+        .i_sig(GAME == 4'hf),
+        .R_E  (GAME_OVER)
+    );
+
+    sound_card u_sound_card (
+        .clk      (clk),
+        .reset    (reset),
+        .game_over(GAME_OVER),
+        .game_on  (get),
+        .speaker  (speaker)
+    );
+
+endmodule
+
+module edgeDT (
+    input  clk,
+    input  i_sig,
+    output R_E
+);
+    logic D0, D1;
+    always_ff @(posedge clk) begin : EDGE_DETECT
+        D0 <= i_sig;
+        D1 <= D0;
+    end
+    assign R_E = D0 & ~D1;
 endmodule
