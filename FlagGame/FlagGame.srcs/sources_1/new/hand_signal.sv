ac7ff96acccdc120b394cfb8508dbc7482d0aa87
pumpk <cong2738@gmail.com>
2025-06-11 03:50:38 +0900

    feat: 화면에서 감지한 깃발 위치를 바로 알 수 있는 기능 추가-> 아직 확인 못함

--- FlagGame/FlagGame.srcs/sources_1/new/hand_signal.sv
+++ FlagGame/FlagGame.srcs/sources_1/new/hand_signal.sv
@@ -1,37 +1,37 @@
 `timescale 1ns / 1ps
 
 module top_hand_signal (
-    input              clk,
-    input              reset,
-    input              game_start,
+    input        clk,
+    input        reset,
+    input        game_start,
     // ov7670 signals
-    input  logic       ov7670_start,
-    output logic       ov7670_scl,
-    output logic       ov7670_sda,
-    output logic       ov7670_xclk,    // == mclk
-    input  logic       ov7670_pclk,
-    input  logic       ov7670_href,
-    input  logic       ov7670_v_sync,
-    input  logic [7:0] ov7670_data,
+    input        ov7670_start,
+    output       ov7670_scl,
+    output       ov7670_sda,
+    output       ov7670_xclk,    // == mclk
+    input        ov7670_pclk,
+    input        ov7670_href,
+    input        ov7670_v_sync,
+    input  [7:0] ov7670_data,
     // export 
-    output logic       Hsync,
-    output logic       Vsync,
-    output logic [3:0] vgaRed,
-    output logic [3:0] vgaGreen,
-    output logic [3:0] vgaBlue,
-    output logic [3:0] fndCom,
-    output logic [7:0] fndFont
+    output       Hsync,
+    output       Vsync,
+    output [3:0] vgaRed,
+    output [3:0] vgaGreen,
+    output [3:0] vgaBlue,
+    output [3:0] fndCom,
+    output [7:0] fndFont
 );
-    SCCB_core u_SCCB_core (
-        .clk          (clk),
-        .reset        (reset),
+    SCCB_core u_SCCB_core(
+        .clk(clk),
+        .reset(reset),
         .initial_start(ov7670_start),
-        .sioc         (ov7670_scl),
-        .siod         (ov7670_sda)
+        .sioc(ov7670_scl),
+        .siod(ov7670_sda)
     );
 
-    logic [3:0] ov7670_Red, ov7670_Green, ov7670_Blue;
-    logic [9:0] x_pixel, y_pixel;
+    wire [3:0] ov7670_Red, ov7670_Green, ov7670_Blue;
+    wire [9:0] x_pixel, y_pixel;
     OV7670_VGA_Display u_OV7670_VGA_Display (
         .clk          (clk),
         .reset        (reset),
@@ -51,12 +51,20 @@ module top_hand_signal (
         .y_pixel      (y_pixel)
     );
 
-    logic [3:0] blue_flag, red_flag;
+    wire [3:0] zone_id;
+    AreaSel u_AreaSel (
+        .x_pixel(x_pixel),
+        .y_pixel(y_pixel),
+        .zone_id(zone_id)
+    );
+
+    wire [3:0] blue_flag, red_flag;
     hand_signal u_hand_signal (
         .clk        (clk),
         .rst        (reset),
         .x_pixel    (x_pixel),
         .y_pixel    (y_pixel),
+        .zone_id    (zone_id),
         .pixel_COLOR({ov7670_Red, ov7670_Green, ov7670_Blue}),
         .blue_flag  (blue_flag),
         .red_flag   (red_flag)
@@ -72,14 +80,16 @@ module top_hand_signal (
     );
 
     print_grid u_print_grid (
-        .R  (ov7670_Red),
-        .G  (ov7670_Green),
-        .B  (ov7670_Blue),
-        .x  (x_pixel),
-        .y  (y_pixel),
-        .o_R(vgaRed),
-        .o_G(vgaGreen),
-        .o_B(vgaBlue)
+        .R        (ov7670_Red),
+        .G        (ov7670_Green),
+        .B        (ov7670_Blue),
+        .x        (x_pixel),
+        .y        (y_pixel),
+        .blue_flag(blue_flag),
+        .red_flag (red_flag),
+        .o_R      (vgaRed),
+        .o_G      (vgaGreen),
+        .o_B      (vgaBlue)
     );
 
 endmodule
@@ -88,18 +98,25 @@ module print_grid #(
     X_SIZE = 640,
     Y_SIZE = 480
 ) (
-    input [3:0] R,
-    input [3:0] G,
-    input [3:0] B,
-    input [9:0] x,
-    input [9:0] y,
+    input        [3:0] R,
+    input        [3:0] G,
+    input        [3:0] B,
+    input        [9:0] x,
+    input        [9:0] y,
+    input        [3:0] blue_flag,
+    input        [3:0] red_flag,
+    input        [3:0] zone_id,
     output logic [3:0] o_R,
     output logic [3:0] o_G,
     output logic [3:0] o_B
 );
     localparam X_UNIT = X_SIZE / 3, Y_UNIT = Y_SIZE / 3;
     always_comb begin : PRINT_LOGIC
-        {o_R, o_G, o_B} = {R, G, B};
+        {o_R, o_G, o_B} = {
+            zone_id != red_flag && zone_id == blue_flag ? 0 : R,
+            zone_id == red_flag || zone_id == blue_flag ? 0 : G,
+            zone_id == red_flag && zone_id != blue_flag ? 0 : B
+        };
         case (x)
             X_UNIT * 1: {o_R, o_G, o_B} = {4'hf, 4'h0, 4'h0};
             X_UNIT * 2: {o_R, o_G, o_B} = {4'hf, 4'h0, 4'h0};
@@ -113,31 +130,14 @@ module print_grid #(
     end
 endmodule
 
-module hand_signal #(
+module AreaSel #(
     parameter IMG_WIDTH  = 640,
     parameter IMG_HEIGHT = 480
 ) (
-    input                               clk,
-    input                               rst,
     input      [ $clog2(IMG_WIDTH)-1:0] x_pixel,
     input      [$clog2(IMG_HEIGHT)-1:0] y_pixel,
-    input      [                  11:0] pixel_COLOR,
-    output reg [                   3:0] blue_flag,
-    output reg [                   3:0] red_flag
+    output reg [                   3:0] zone_id
 );
-    reg [3:0] max_zone_color1, max_zone_color2;
-
-    integer i;
-
-    // RGB 분리
-    wire [3:0] R = pixel_COLOR[11:8];
-    wire [3:0] G = pixel_COLOR[7:4];
-    wire [3:0] B = pixel_COLOR[3:0];
-
-    // 색 조건
-    wire is_color1 = (R < 10) && (G < 10) && (10 < B);  //blue 계열
-    wire is_color2 = (G < 10) && (B < 10) && (10 < R);  //red 계열
-
     // 영역 경계 계산
     localparam X_UNIT = IMG_WIDTH / 3;
     localparam Y_UNIT = IMG_HEIGHT / 3;
@@ -150,7 +150,6 @@ module hand_signal #(
             Y_AREA2 = y_pixel < (Y_UNIT * 3);
 
     // 영역 판별 (3x3 기준)
-    reg [3:0] zone_id;
     always @(*) begin
         if (X_AREA0) begin
             if (Y_AREA0) zone_id = 4'd0;
@@ -166,6 +165,37 @@ module hand_signal #(
             else if (Y_AREA2) zone_id = 4'd8;
         end
     end
+endmodule
+
+module hand_signal #(
+    parameter IMG_WIDTH = 640,
+    parameter IMG_HEIGHT = 480,
+    parameter IMG_WB = $clog2(IMG_WIDTH),
+    parameter IMG_HB = $clog2(IMG_HEIGHT)
+) (
+    input                   clk,
+    input                   rst,
+    input      [IMG_WB-1:0] x_pixel,
+    input      [IMG_HB-1:0] y_pixel,
+    input      [       3:0] zone_id,
+    input      [      11:0] pixel_COLOR,
+    output reg [       3:0] blue_flag,
+    output reg [       3:0] red_flag
+);
+    reg [3:0] max_zone_color1, max_zone_color2;
+
+    integer i;
+
+    // RGB 분리
+    wire [3:0] R = pixel_COLOR[11:8];
+    wire [3:0] G = pixel_COLOR[7:4];
+    wire [3:0] B = pixel_COLOR[3:0];
+
+    // 색 조건
+    wire is_color1 = (R < 10) && (G < 10) && (10 < B);  //blue 계열
+    wire is_color2 = (G < 10) && (B < 10) && (10 < R);  //red 계열
+
+
     // 색별 영역 카운트 배열
     reg [31:0] zone_count_color1[0:8];
     reg [31:0] zone_count_color2[0:8];
