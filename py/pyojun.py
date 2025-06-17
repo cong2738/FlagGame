7d7d294af15af1ff3986bd053a3ab6e401d81a12
pumpk <cong2738@gmail.com>
2025-06-17 11:57:19 +0900

    feat: 평균, 표준편자 파일 열기 경로 변경 - os 라이브러리 사용, 해당 py파일 경로에서부터 찾음

--- py/pyojun.py
+++ py/pyojun.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt  # 히스토그램을 위한 라이브러리
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))  # py파일이 있는 디렉토리
+file_path = os.path.join(base_dir, "rnd_output.txt")
 
 # 파일에서 16진수 난수 읽기
-with open("./rnd_output.txt", "r") as f:
-    hex_values = [line.strip() for line in f if line.strip()]
+try:
+    with open(file_path, "r") as f:
+        hex_values = [line.strip() for line in f if line.strip()]
+except:
+    print("File Open Error...")
 
 # 정수로 변환
 int_values = [int(val, 16) for val in hex_values]
@@ -24,4 +31,3 @@ plt.ylabel("Frequency", fontsize=13)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-
